f7a5f053baa09a171d80b8bf846c0b80c9bddb69
nasirdiu <nasir.joypurhat@gmail.com>
2022-03-16 01:07:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const singer=[
+  {name:'Dev',job:'singer'},
+  {name:'agun',job:'singer'},
+  {name:'Tufan',job:'Movie name'},
+  {name:'Prothik nobi',job:'singer'},
+
+]
 function App() {
+  const nayoks = ["Nasir Uddin", "Shouel",'Josim','Hero Alom','Riyaj','Razzak' ,'Farok'];
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok=><li>Name:{nayok}</li>)
+      }
+      {
+        // nayoks.map(nayok=> <Person name={nayok}></Person>)
+      }
+
+      {
+        singer.map(sing=><Person name={sing.name}></Person>)
+      }
+
+      {/* <Person name={nayoks[0]} nayika="Mim"></Person>
+      <Person name={nayoks[1]} nayika="Mayzabin"></Person> */}
+      <h5>New Compoent ba new tag</h5>
+      <p>Rock mama React mama</p>
+      <Person1 phone="017458552" name="Pallob Gandu"></Person1>
+      <Person1 phone="01235974" name="Arif"></Person1>
     </div>
   );
 }
-
+function Person(props) {
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  );
+}
+function Person1(props) {
+  return (
+    <div className="person1">
+      <h3>Name:{props.name}</h3>
+      <p>Phone:{props.phone}</p>
+    </div>
+  );
+}
 export default App;
